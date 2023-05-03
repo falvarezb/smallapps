f9d7d668bfc5396105d77a9f0126ea28f12323d1
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-05-03 17:11:24 +0100

    round robin distribution

--- openmp/par_pi.c
+++ openmp/par_pi.c
@@ -8,20 +8,19 @@
 #define NUM_STEPS 5000000000L
 #define NUM_THREADS 256
 
-void thread_body(double *sum, double step) {
+void thread_body(double *sum, double step, int *actual_num_threads) {
     int num_threads = omp_get_num_threads();
     int id = omp_get_thread_num();
-    // printf(" thread(%d)\n", id);
-    size_t init_num_steps = NUM_STEPS / num_threads * id;
-    size_t end_num_steps = NUM_STEPS / num_threads * (id + 1);
-    // in case NUM_THREADS does not divide NUM_STEPS, last thread takes the extra work
-    if(id == NUM_THREADS - 1 && end_num_steps < NUM_STEPS) {
-        end_num_steps = NUM_STEPS;
+
+    //we choose a random thread to update actual_num_threads
+    if(id == 0) {
+        *actual_num_threads = num_threads;        
     }
 
-    // printf(" thread(%d) - init_NUM_STEPS(%ld)\n", id, init_num_steps);
-    // printf(" thread(%d) - end_NUM_STEPS(%ld)\n", id, end_num_steps);
-    for(size_t i = init_num_steps; i < end_num_steps; i++) {
+    // printf(" thread(%d)\n", id);
+
+    //round robin distribution of the thread's job
+    for(size_t i = id; i < NUM_STEPS; i += num_threads) {
         double x = (i + 0.5) * step;
         sum[id] += 4.0 / (1.0 + x * x);
     }
@@ -29,7 +28,10 @@ void thread_body(double *sum, double step) {
 
 int main(int argc, char const *argv[]) {
     double step = 1.0 / NUM_STEPS;
+
+    //CAUTION: the environment can choose to create fewer threads than requested
     omp_set_num_threads(NUM_THREADS);
+    int actual_num_threads;
     double sum[NUM_THREADS] = { 0 };
     printf("NUM_STEPS=%ld\n", NUM_STEPS);
     printf("NUM_THREADS=%d\n", NUM_THREADS);
@@ -37,17 +39,16 @@ int main(int argc, char const *argv[]) {
 
 #pragma omp parallel
     {
-        thread_body(sum, step);
+        thread_body(sum, step, &actual_num_threads);
     }
 
     double total_sum = 0;
-    for(int i = 0; i < NUM_THREADS; i++) {
+    for(int i = 0; i < actual_num_threads; i++) {
         total_sum += sum[i];
     }
 
     double pi = step * total_sum;
+    printf("actual num threads=%d\n", actual_num_threads);
     printf("time=%0.3f\n", omp_get_wtime() - start_time);
     printf("pi=%0.20f\n", pi);
 }
-
-
