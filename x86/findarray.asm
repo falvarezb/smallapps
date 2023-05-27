22ee9a323c1d7bb11cc7d8a497748abda3cd420a
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2023-05-27 19:09:36 +0100

    findarray datatype change

--- x86/findarray.asm
+++ x86/findarray.asm
@@ -1,6 +1,11 @@
 ;
 ; find first element in array of integers that is equal to 0
 ;
+; Comment on data types
+; - if we use 64-bit data types, then int_array is 'dq' and array_elem_size is 8 and we can use register 'rbx'
+; - however, if we use different data types, like 'dw', then array_elem_size is 2 and we need to use 'bx' to make
+;   sure the zero flag (ZF) is set based on the value of lowest 2 bytes
+;
 
         global     _main
         default rel
@@ -10,9 +15,9 @@ _main:
         mov rcx,array_length    ; implicit loop counter        
 
 lp:                             ; loop to traverse array
-        mov rbx,-1              ; register to store the result: if 0 is found, rbx=0, otherwise rbx!=0
+        mov bx,-1              ; register to store the result: if 0 is found, rbx=0, otherwise rbx!=0
                                 ; initialized to identity element of 'and'
-        and rbx,[rdi]
+        and bx,[rdi]
         je exit
         add rdi,array_elem_size ; advancing array pointer to the next element
         loop lp
@@ -24,6 +29,6 @@ exit:
         
 
         section .data  
-array_elem_size equ     8  ; size in bytes (this value must correspond to the datatype of int_array)
-int_array       dq      1,6,0,3
+array_elem_size equ     2  ; size in bytes (this value must correspond to the datatype of int_array)
+int_array       dw      1,6,0,3
 array_length    equ     ($-int_array)/array_elem_size        
